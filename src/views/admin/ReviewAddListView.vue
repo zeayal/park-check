ee61f5703ba4f16a7aa5bb792258061769fcd9d7
daisydinglady <52791746+daisydinglady@users.noreply.github.com>
2025-07-28 23:52:29 +0800

    调整模态框展示宽度

--- src/views/admin/ReviewAddListView.vue
+++ src/views/admin/ReviewAddListView.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="container">
-    <ReviewListView mode="add" title="新增营地管理" v-model:loading="loading" />
+    <ReviewListView
+      mode="add"
+      title="新增营地管理"
+      v-model:loading="loading"
+      class="custom-modal"
+    />
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-import ReviewListView from '@/components/review/ReviewListView.vue';
+import { ref } from "vue";
+import ReviewListView from "@/components/review/ReviewListView.vue";
 
 const loading = ref(false);
-
 </script>
+
+<style scoped>
+::v-deep.custom-modal .el-dialog {
+  width: 50% !important;
+}
+
+@media screen and (max-width: 768px) {
+  ::v-deep.custom-modal .el-dialog {
+    width: 90% !important;
+  }
+}
+</style>
--- src/views/admin/ReviewEditListView.vue
+++ src/views/admin/ReviewEditListView.vue
@@ -4,6 +4,7 @@
       mode="edit"
       title="修改营地管理"
       v-model:loading="loading"
+      class="custom-modal"
     />
   </div>
 </template>
@@ -13,3 +14,16 @@ import ReviewRevisionListView from "@/components/review/ReviewRevisionListView.v
 
 const loading = ref(false);
 </script>
+
+<style scoped>
+::v-deep.custom-modal .el-dialog {
+  width: 50% !important;
+}
+
+@media screen and (max-width: 768px) {
+  /* 若组件是基于 el-dialog 封装，直接修改 el-dialog 样式 */
+  ::v-deep.custom-modal .el-dialog {
+    width: 90% !important; /* 手机端占比更大，如 90% */
+  }
+}
+</style>
